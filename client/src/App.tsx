88857213dfd70ee215aecf73d7e6514c0ccb71e1
choi-jiae <chlwldo2002@gmail.com>
2025-08-02 22:49:50 +0900

    chore: App 파일 초기화 - 버튼 테스트 한 부분이 잘못 커밋되어 초기화

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,7 @@
-import { Button } from "@/components/common/button";
-
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-2">
-      <Button>이 강좌 신청하기</Button>
-      <Button variant={"outline"}>다음 기수 신청하기</Button>
-      <Button disabled>신청 완료</Button>
-      <Button variant={"outline"} size={"lgFlexible"} textSize={"title2"}>
-        전체 강좌 둘러보기
-      </Button>
-      <Button size={"md"}>강좌 신청하기</Button>
-      <Button variant={"hoverUp"} size={"sm"}>+ 강의 업로드 하기</Button>
-      <Button variant={"ghost"} size={"xs"}>
-        로그아웃
-      </Button>
-    </div>
+    <>
+      <h1>App</h1>
+    </>
   );
 }
-
-export default App;
