716c12a505b5f4c7a39ad25df9384cab5906be2e
hkjbrian <hkjbrian@naver.com>
2025-08-04 19:34:37 +0900

    fix: Repository 저장 오브젝트 수정

--- server/src/main/java/com/seniclass/server/domain/lecture/repository/UploadTimeRepository.java
+++ server/src/main/java/com/seniclass/server/domain/lecture/repository/UploadTimeRepository.java
@@ -1,6 +1,6 @@
 package com.seniclass.server.domain.lecture.repository;
 
-import com.seniclass.server.domain.student.domain.Student;
+import com.seniclass.server.domain.lecture.domain.UploadTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UploadTimeRepository extends JpaRepository<Student, Long> {}
+public interface UploadTimeRepository extends JpaRepository<UploadTime, Long> {}
