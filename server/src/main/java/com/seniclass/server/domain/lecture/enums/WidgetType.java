c0dc4e38e4baa14362740fc40d53891a655a2782
hkjbrian <hkjbrian@naver.com>
2025-08-04 20:17:00 +0900

    feat: WidgetSetting class 추가 및 WidgetType enum 정의 - 정적 팩토리 메서드를 통해서만 WidgetSetting 객체 생성할 수 있도록 함

---
+++ server/src/main/java/com/seniclass/server/domain/lecture/enums/WidgetType.java
@@ -0,0 +1,10 @@
+package com.seniclass.server.domain.lecture.enums;
+
+public enum WidgetType {
+    QNA,
+    SUBMISSION,
+    TEACHER_INFO,
+    REVIEW,
+    NEXT_LECTURE,
+    STUDENTS_STATUS,
+}
