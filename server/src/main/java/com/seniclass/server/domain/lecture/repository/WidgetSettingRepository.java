4179823b6491eb008d48b8b2c1731f6cb739358f
hkjbrian <hkjbrian@naver.com>
2025-08-04 20:18:35 +0900

    feat: WidgetSetting 레포지토리 추가 - Spring Data JPA 활용한 Repository 생성

---
+++ server/src/main/java/com/seniclass/server/domain/lecture/repository/WidgetSettingRepository.java
@@ -0,0 +1,6 @@
+package com.seniclass.server.domain.lecture.repository;
+
+import com.seniclass.server.domain.lecture.domain.WidgetSetting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WidgetSettingRepository extends JpaRepository<WidgetSetting, Integer> {}
