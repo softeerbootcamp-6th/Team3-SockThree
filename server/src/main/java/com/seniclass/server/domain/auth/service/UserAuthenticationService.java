50418d5b64c312af5fe303679f98bf7649e97630
coffeesigma <ljb22060@gmail.com>
2025-08-05 02:21:14 +0900

    feat: User 모델 사용하는 인증 유저 모델의 인증/인가 등 서비스 구현

---
+++ server/src/main/java/com/seniclass/server/domain/auth/service/UserAuthenticationService.java
@@ -0,0 +1,18 @@
+package com.seniclass.server.domain.auth.service;
+
+import com.seniclass.server.domain.auth.domain.AuthenticatedUser;
+import com.seniclass.server.domain.student.enums.Gender;
+
+public interface UserAuthenticationService {
+
+  AuthenticatedUser authenticate(String email, String password);
+
+  AuthenticatedUser findByIdAndValidate(String userId);
+
+  AuthenticatedUser findByEmailAndValidate(String email);
+
+  String createStudent(
+      String name, String email, Integer age, Gender gender, String encodedPassword);
+
+  boolean existsByEmail(String email);
+}
