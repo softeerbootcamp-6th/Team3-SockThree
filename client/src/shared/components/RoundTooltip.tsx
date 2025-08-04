e28633bbbdd11beeab42487a74c7664f594f1f6c
choi-jiae <chlwldo2002@gmail.com>
2025-08-05 00:38:28 +0900

    chore: svg 파일명 변경 및 주석 삭제

--- client/src/shared/components/RoundTooltip.tsx
+++ client/src/shared/components/RoundTooltip.tsx
@@ -1,39 +1,4 @@
-// import RoundToolTipSVG from "@/assets/icons/tooltip/tooltip_round.svg?react";
-// import * as React from "react";
-
-// // 툴팁 속성 정의
-// interface RoundTooltipProps {
-//   text: string;
-// }
-
-// const RoundTooltip = ({ text }: RoundTooltipProps) => {
-//   return (
-//     <RoundTooltipContainer>
-//       <div className="flex flex-col gap-[0.4rem]">
-//         <span className="typo-label-1 break-words whitespace-normal text-gray-800">
-//           {text}
-//         </span>
-//       </div>
-//     </RoundTooltipContainer>
-//   );
-// };
-
-// interface RoundTooltipContainerProps {
-//   children: React.ReactNode;
-// }
-
-// const RoundTooltipContainer = ({ children }: RoundTooltipContainerProps) => {
-//   return (
-//     <div className="relative inline-block w-[9.5625rem]">
-//       <RoundToolTipSVG className="block" />
-//       <div className={`flex px-[1.5rem] pt-[.75rem]`}>{children}</div>
-//     </div>
-//   );
-// };
-
-// export default RoundTooltip;
-
-import RoundToolTipSVG from "@/assets/icons/tooltip/tooltip_round.svg?react";
+import RoundToolTipSVG from "@/assets/icons/tooltip/tooltip-round.svg?react";
 import * as React from "react";
 
 interface RoundTooltipProps {
