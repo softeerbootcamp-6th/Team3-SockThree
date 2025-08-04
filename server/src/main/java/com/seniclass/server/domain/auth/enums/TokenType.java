287f8378439603b3f909b6f14ccd0eb24a56db57
coffeesigma <ljb22060@gmail.com>
2025-08-05 02:17:23 +0900

    feat: 토큰 타입 및 인증 오류 정의

---
+++ server/src/main/java/com/seniclass/server/domain/auth/enums/TokenType.java
@@ -0,0 +1,16 @@
+package com.seniclass.server.domain.auth.enums;
+
+public enum TokenType {
+  ACCESS("ACCESS"),
+  REFRESH("REFRESH");
+
+  private final String value;
+
+  TokenType(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+}
