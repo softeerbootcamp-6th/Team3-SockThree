0c479d18604809af42009458fdd252139851b6d5
hkjbrian <hkjbrian@naver.com>
2025-08-05 15:53:49 +0900

    feat: Chapter 도메인 기본 설정 - Chapter 엔티티 생성 - Chapter 레포지토리 생성 - Lecture 와 연관관계 매핑

---
+++ server/src/main/java/com/seniclass/server/domain/lecture/domain/Chapter.java
@@ -0,0 +1,40 @@
+package com.seniclass.server.domain.lecture.domain;
+
+import com.seniclass.server.domain.common.model.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chapter extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chapter_id", nullable = false)
+    private Long id;
+
+    @Column(name = "chapter_name", nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lecture_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Lecture lecture;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    public Chapter(String name, Lecture lecture) {
+        this.name = name;
+        this.lecture = lecture;
+    }
+
+    public static Chapter createChapter(String name, Lecture lecture) {
+        return Chapter.builder().name(name).lecture(lecture).build();
+    }
+}
+
