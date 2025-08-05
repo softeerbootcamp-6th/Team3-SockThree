bba71e92a0e15c39ce51412e300f8c398381e14a
coffeesigma <ljb22060@gmail.com>
2025-08-05 18:22:16 +0900

    fix: repository 수정

--- server/src/main/java/com/seniclass/server/domain/category/repository/SubCategoryRepository.java
+++ server/src/main/java/com/seniclass/server/domain/category/repository/SubCategoryRepository.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubCategoryRepository extends JpaRepository<SubCategory, Integer> {}
+public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {}
