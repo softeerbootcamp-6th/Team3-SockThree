d8e65a3b7d50f7ea2925c25dd403d681519c50bb
hkjbrian <hkjbrian@naver.com>
2025-08-05 17:21:06 +0900

    feat: PATH enum 생성 - 경로 상수 관리를 위한 enum 생성

---
+++ server/src/main/java/com/seniclass/server/global/enums/PATH.java
@@ -0,0 +1,17 @@
+package com.seniclass.server.global.enums;
+
+public enum PATH {
+    CDN_STREAMING_PATH("https://cdn.senicle.com/"),
+
+    ;
+
+    final String path;
+
+    PATH(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
