f727e93096f46d51773d7d5772b029a11fd3a9c0
hkjbrian <hkjbrian@naver.com>
2025-08-05 14:24:05 +0900

    fix: Repository 애노테이션 누락 수정

--- server/src/main/java/com/seniclass/server/domain/lecture/repository/LectureRepository.java
+++ server/src/main/java/com/seniclass/server/domain/lecture/repository/LectureRepository.java
@@ -2,5 +2,7 @@ package com.seniclass.server.domain.lecture.repository;
 
 import com.seniclass.server.domain.lecture.domain.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {}
