e005d21cdddac41a0e44703e76da0aad7c5e0f92
coffeesigma <ljb22060@gmail.com>
2025-08-05 18:17:13 +0900

    feat: Category entity/repository 생성 및 카테고리 예시 생성

---
+++ server/src/main/java/com/seniclass/server/domain/category/domain/SubCategory.java
@@ -0,0 +1,27 @@
+package com.seniclass.server.domain.category.domain;
+
+import com.seniclass.server.domain.common.model.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SubCategory extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sub_category_id", nullable = false)
+    private Long id;
+
+    @Column(name = "sub_category_name", nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "main_category_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private MainCategory mainCategory;
+}
