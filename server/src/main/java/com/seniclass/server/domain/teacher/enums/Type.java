8a1ded7d10376c71c84bb3c090f31c8719442a6b
hkjbrian <hkjbrian@naver.com>
2025-08-06 19:42:40 +0900

    feat: Career 도메인 구현 - Career 엔티티 생성 - 이력의 종류를 포함하는 Type Enum 생성 - Repository 생성

---
+++ server/src/main/java/com/seniclass/server/domain/teacher/enums/Type.java
@@ -0,0 +1,7 @@
+package com.seniclass.server.domain.teacher.enums;
+
+public enum Type {
+    EXPERIENCE,
+    CERTIFICATE,
+
+}
---
+++ server/src/main/java/com/seniclass/server/domain/teacher/repository/CareerRepository.java
@@ -0,0 +1,9 @@
+package com.seniclass.server.domain.teacher.repository;
+
+import com.seniclass.server.domain.teacher.domain.Career;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CareerRepository extends JpaRepository<Career, Long> {
+}
