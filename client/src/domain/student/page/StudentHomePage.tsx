e2c8773820a974a760a862a1ef2b2958ceeb773a
choi-jiae <chlwldo2002@gmail.com>
2025-08-06 15:05:31 +0900

    style: student home page 수정

--- client/src/domain/student/page/StudentHomePage.tsx
+++ client/src/domain/student/page/StudentHomePage.tsx
@@ -7,12 +7,12 @@ import Button from "@/shared/components/Button";
 
 const StudentHomePage = () => {
   return (
-    <div className="w-full pt-[4rem] pr-[3.5625rem] pl-[2.8125rem]">
+    <div className="h-[100%] w-[100%] pt-[4rem] pr-[3.5625rem] pb-[3.75rem] pl-[2.8125rem]">
       <div className="flex w-full items-center justify-between pb-[2.6875rem]">
         <div>
           <div className="flex items-center gap-[1.1875rem]">
             <span className="typo-title-0 whitespace-nowrap">마감까지</span>
-            <CheckIcon className="-z-10 h-[4.0625rem]" />
+            <CheckIcon className="h-[4.0625rem]" />
           </div>
           <span className="typo-title-0 whitespace-nowrap">
             얼마 남지 않았어요!
@@ -24,13 +24,11 @@ const StudentHomePage = () => {
       </div>
       <HoverExpandCourseCardView />
       <div className="mt-[3.8125rem] mb-[2.8125rem] flex h-[.0625rem] w-[91.6875rem] bg-gray-500" />
-      <div className="flex flex-row justify-between">
+      <div className="flex h-[21.5rem] w-[92.1875rem] flex-row items-end justify-between">
         <RecommendedCourseListView />
-        <div className="item-start flex h-[22.0625rem]">
-          <Button variant={"outline"} textSize={"title2"}>
-            전체 강좌 둘러보기
-          </Button>
-        </div>
+        <Button variant={"outline"} textSize={"title2"}>
+          전체 강좌 둘러보기
+        </Button>
       </div>
     </div>
   );
