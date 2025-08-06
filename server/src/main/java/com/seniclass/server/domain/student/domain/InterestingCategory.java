491ca1ef36f620c98f0ece87815d42f9ca3aaf05
coffeesigma <ljb22060@gmail.com>
2025-08-06 14:57:34 +0900

    feat: 수강생 관련 남은 엔티티 정의

---
+++ server/src/main/java/com/seniclass/server/domain/student/domain/InterestingCategory.java
@@ -0,0 +1,37 @@
+package com.seniclass.server.domain.student.domain;
+
+import com.seniclass.server.domain.category.domain.SubCategory;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InterestingCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "interesting_cagetory_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "interesting_category_id", nullable = false)
+    private SubCategory subCategory;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private InterestingCategory(Student student, SubCategory subCategory) {
+        this.student = student;
+        this.subCategory = subCategory;
+    }
+
+    public static InterestingCategory createInterestingCategory(
+            Student student, SubCategory subCategory) {
+        return InterestingCategory.builder().student(student).subCategory(subCategory).build();
+    }
+}
