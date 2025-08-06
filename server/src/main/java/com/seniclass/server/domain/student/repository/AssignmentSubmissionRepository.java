90ba854b8703a9000291ec4df253e672e56d900b
coffeesigma <ljb22060@gmail.com>
2025-08-06 15:03:23 +0900

    feat: 생성한 entity 에 대한 Repository 생성

---
+++ server/src/main/java/com/seniclass/server/domain/student/repository/AssignmentSubmissionRepository.java
@@ -0,0 +1,9 @@
+package com.seniclass.server.domain.student.repository;
+
+import com.seniclass.server.domain.student.domain.AssignmentSubmission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssignmentSubmissionRepository
+        extends JpaRepository<AssignmentSubmission, Integer> {}
