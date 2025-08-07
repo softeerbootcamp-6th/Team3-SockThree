4b083777459d5c3d6edec1ccccaf8e0458f6c824
yyeonzu <yyeonzu@gmail.com>
2025-08-07 18:35:40 +0900

    chore: 폴더명 소문자로 변경

--- client/src/domain/instructor/component/funnel/FunnelForm.tsx
+++ client/src/domain/instructor/component/funnel/FunnelForm.tsx
@@ -1,13 +1,13 @@
-import StepCategory from "@/domain/instructor/component/Step/StepCategory.tsx";
-import StepSubCategory from "@/domain/instructor/component/Step/StepSubCategory.tsx";
-import StepLevel from "@/domain/instructor/component/Step/StepLevel.tsx";
-import StepDateRange from "@/domain/instructor/component/Step/StepDateRange.tsx";
-import StepMaxHeadCount from "@/domain/instructor/component/Step/StepMaxHeadCount.tsx";
-import StepUploadTimes from "@/domain/instructor/component/Step/StepUploadTimes.tsx";
-import StepPrice from "@/domain/instructor/component/Step/StepPrice.tsx";
-import StepIntroduction from "@/domain/instructor/component/Step/StepIntroduction.tsx";
-import StepCurriculum from "@/domain/instructor/component/Step/StepCurriculum.tsx";
-import StepImageUpload from "@/domain/instructor/component/Step/StepImageUpload.tsx";
+import StepCategory from "@/domain/instructor/component/step/StepCategory.tsx";
+import StepSubCategory from "@/domain/instructor/component/step/StepSubCategory.tsx";
+import StepLevel from "@/domain/instructor/component/step/StepLevel.tsx";
+import StepDateRange from "@/domain/instructor/component/step/StepDateRange.tsx";
+import StepMaxHeadCount from "@/domain/instructor/component/step/StepMaxHeadCount.tsx";
+import StepUploadTimes from "@/domain/instructor/component/step/StepUploadTimes.tsx";
+import StepPrice from "@/domain/instructor/component/step/StepPrice.tsx";
+import StepIntroduction from "@/domain/instructor/component/step/StepIntroduction.tsx";
+import StepCurriculum from "@/domain/instructor/component/step/StepCurriculum.tsx";
+import StepImageUpload from "@/domain/instructor/component/step/StepImageUpload.tsx";
 
 import { useFunnelScroll } from "@/domain/instructor/hook/useFunnellScroll.ts";
 import * as React from "react";
