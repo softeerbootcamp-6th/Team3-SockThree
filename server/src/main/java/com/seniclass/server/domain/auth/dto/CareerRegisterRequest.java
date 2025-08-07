f5d33bd441c04799eab1354b954bd09c41e4f14c
hkjbrian <hkjbrian@naver.com>
2025-08-07 13:46:01 +0900

    feat: 강사 회원가입 Request dto 구현 - CareerRegisterRequest ( 커리어 정보 ) - TeacherRegisterRequest ( 강사 회원 정보 )

---
+++ server/src/main/java/com/seniclass/server/domain/auth/dto/CareerRegisterRequest.java
@@ -0,0 +1,19 @@
+package com.seniclass.server.domain.auth.dto;
+
+import com.seniclass.server.domain.teacher.enums.Type;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Schema(description = "강사 이력")
+public record CareerRegisterRequest(
+        @Schema(description = "이력", example = "생활체육지도자 1급")
+                @NotBlank(message = "이력 이름은 필수입니다.")
+                @Size(max = 50, message = "이력은 50자를 넘을 수 없습니다.")
+                String name,
+        @Schema(
+                        description = "이력 종류, [ EXPERIENCE : 일반 경력, CERTIFICATE : 자격증 ]",
+                        example = "EXPERIENCE",
+                        allowableValues = {"EXPERIENCE", "CERTIFICATE"})
+                @NotBlank(message = "이력 종류는 필수입니다.")
+                Type type) {}
