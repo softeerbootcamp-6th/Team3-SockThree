a289942851e559f6c23795b1354f32a13065025f
yyeonzu <yyeonzu@gmail.com>
2025-08-07 17:44:16 +0900

    chore: 프리티어 적용

--- client/src/domain/instructor/hook/useFunnellScroll.ts
+++ client/src/domain/instructor/hook/useFunnellScroll.ts
@@ -1,19 +1,14 @@
 import { useEffect, useRef } from "react";
 
-export const useFunnelScroll = ({
-  stepIndex
-}: {
-  stepIndex: number;
-}) => {
+export const useFunnelScroll = ({ stepIndex }: { stepIndex: number }) => {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const stepRef = useRef<HTMLElement | null>(null);
-
 
   useEffect(() => {
     requestAnimationFrame(() => {
       stepRef.current?.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
+        behavior: "smooth",
+        block: "start",
       });
     });
   }, [stepIndex]);
