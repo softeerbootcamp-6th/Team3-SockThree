8fa22c74b0fcb4d86cb14d41ead3003a0d162e77
yyeonzu <yyeonzu@gmail.com>
2025-08-07 13:52:19 +0900

    chore: 추론 가능한 타입 명시 삭제, 파일 위치 이동

--- client/src/domain/instructor/page/CreateCoursePage.tsx
+++ client/src/domain/instructor/page/CreateCoursePage.tsx
@@ -1,13 +1,50 @@
 import FunnelForm from "@/domain/instructor/component/funnel/FunnelForm.tsx";
 import { FormProvider, useForm } from "react-hook-form";
-import {
-  type FunnelFormSchema,
-  funnelSchema,
-} from "@/domain/instructor/schema/funnelSchema.ts";
+import { funnelSchema } from "@/domain/instructor/schema/funnelSchema.ts";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useFunnel } from "@/domain/instructor/hook/useFunnel.ts";
 import { FunnelSideBar } from "@/domain/instructor/component/funnel/FunnelSideBar.tsx";
 import FunnelHeader from "@/domain/instructor/component/funnel/FunnelHeader.tsx";
+
+const steps = [
+  { key: "category", label: "대분류" },
+  { key: "subCategory", label: "소분류" },
+  { key: "level", label: "난이도" },
+  { key: "duration", label: "강의기간" },
+  { key: "maxHeadCount", label: "최대 인원" },
+  { key: "uploadTimes", label: "업로드 일정" },
+  { key: "price", label: "수강료" },
+  { key: "introduction", label: "강좌 소개" },
+  { key: "curriculum", label: "커리큘럼" },
+  { key: "imageUrl", label: "사진" },
+] as const;
+
+const CreateCoursePage = () => {
+  const methods = useForm({
+    resolver: zodResolver(funnelSchema),
+    mode: "onChange",
+  });
+
+  const { goNextStep, currentIndex } = useFunnel(steps.map((s) => s.key));
+
+  return (
+    <FormProvider {...methods}>
+      <div className="flex min-h-screen flex-col pr-[5rem] pl-[10rem]">
+        <FunnelHeader currentIndex={currentIndex} totalSteps={steps.length} />
+        <main className="flex flex-row gap-7">
+          <FunnelForm
+            steps={steps}
+            currentStepIndex={currentIndex}
+            goNextStep={goNextStep}
+          />
+          <FunnelSideBar steps={steps} currentStepIndex={currentIndex} />
+        </main>
+      </div>
+    </FormProvider>
+  );
+};
+
+export default CreateCoursePage;
 
 export type Context = {
   category?: string;
@@ -28,48 +65,3 @@ export type Context = {
   curriculum?: string;
   imageUrl?: string;
 };
-
-const steps = [
-  { key: "category", label: "대분류" },
-  { key: "subCategory", label: "소분류" },
-  { key: "level", label: "난이도" },
-  { key: "duration", label: "강의기간" },
-  { key: "maxHeadCount", label: "최대 인원" },
-  { key: "uploadTimes", label: "업로드 일정" },
-  { key: "price", label: "수강료" },
-  { key: "introduction", label: "강좌 소개" },
-  { key: "curriculum", label: "커리큘럼" },
-  { key: "imageUrl", label: "사진" },
-] as const;
-
-const CreateCoursePage = () => {
-  const methods = useForm<FunnelFormSchema>({
-    resolver: zodResolver(funnelSchema),
-    mode: "onChange",
-  });
-
-  const { goNextStep, currentIndex } = useFunnel<Context>(
-    steps.map((s) => s.key)
-  );
-
-  return (
-    <div className="flex min-h-screen flex-col pr-[5rem] pl-[10rem]">
-      <FormProvider {...methods}>
-        <FunnelHeader currentIndex={currentIndex} totalSteps={steps.length} />
-        <main className="flex flex-row gap-7">
-          <FunnelForm<Context>
-            steps={steps}
-            currentStepIndex={currentIndex}
-            goNextStep={goNextStep}
-          />
-          <FunnelSideBar<Context>
-            steps={steps}
-            currentStepIndex={currentIndex}
-          />
-        </main>
-      </FormProvider>
-    </div>
-  );
-};
-
-export default CreateCoursePage;
