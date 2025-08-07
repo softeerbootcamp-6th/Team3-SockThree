a9900616aa8452841dc114b4d1cbc56238e2c3aa
choi-jiae <chlwldo2002@gmail.com>
2025-08-08 00:46:02 +0900

    style: 작은 강의 카드 누르면 해당 페이지로 이동

--- client/src/domain/student/component/mypage/SmallCourseCard.tsx
+++ client/src/domain/student/component/mypage/SmallCourseCard.tsx
@@ -4,18 +4,21 @@ interface SmallCourseCardProps {
   subTitle: string;
   title: string;
   courseThumbnail: string;
+  onClick?: () => void;
 }
 const SmallCourseCard = ({
   subTitle,
   title,
   courseThumbnail,
+  onClick,
 }: SmallCourseCardProps) => {
   return (
     <div
-      className="relative flex h-[28.0956rem] w-[22.625rem] cursor-pointer items-end rounded-[1.25rem] hover:shadow-main"
+      className="relative flex h-[28.0956rem] w-[22.625rem] cursor-pointer items-end rounded-[1.25rem] transition-transform duration-300 ease-in-out hover:-translate-y-2.5 hover:shadow-main"
       style={{
         background: `url(${courseThumbnail})`,
       }}
+      onClick={onClick}
     >
       <div className="absolute inset-0 flex flex-col items-end justify-between rounded-[1.25rem] bg-gradient-to-b from-transparent to-black/60 p-[1.9375rem] px-[1.25rem] py-[1.9238rem]">
         <RightArrowIcon className="h-[1.5rem] w-[1.5rem] text-white" />
