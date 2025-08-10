fdbcdadccc62278cda203d38637f599ab4893661
hkjbrian <hkjbrian@naver.com>
2025-08-10 17:37:02 +0900

    feat: AWS SDK config 작성

---
+++ server/src/main/java/com/seniclass/server/global/config/aws/AwsConfig.java
@@ -0,0 +1,34 @@
+package com.seniclass.server.global.config.aws;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+@RequiredArgsConstructor
+public class AwsConfig {
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                // 자격 증명은 기본 제공 체인(DefaultCredentialsProvider)을 그대로 사용
+                .build();
+    }
+
+    // Presigned URL 생성용
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .build();
+    }
+}
+
