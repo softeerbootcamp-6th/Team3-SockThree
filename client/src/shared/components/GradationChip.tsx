8559304bd621c71a7846558d2fbe0e4e1e8fe8b8
yyeonzu <yyeonzuu@gmail.com>
2025-08-10 14:03:01 +0900

    style: border radius, 색상 수정

--- client/src/shared/components/GradationChip.tsx
+++ client/src/shared/components/GradationChip.tsx
@@ -4,7 +4,7 @@ interface GradationChipProps {
 
 const GradationChip = ({ children }: GradationChipProps) => {
   return (
-    <div className="flex items-center rounded-full border-[.0625rem] border-main-400 bg-gradient-to-br from-[#86DED6]/30 via-[#26CABA]/50 to-[#E5F1F1] px-[var(--spacing-12)] py-[var(--spacing-4)]">
+    <div className="flex items-center rounded-10 border-[.0625rem] border-main-400 bg-[linear-gradient(322deg,rgba(134,222,214,0.15)_0%,rgba(38,202,186,0.25)_46.64%,rgba(229,241,241,0.50)_97.99%)] px-[0.4rem] py-[0.4rem]">
       {children}
     </div>
   );
