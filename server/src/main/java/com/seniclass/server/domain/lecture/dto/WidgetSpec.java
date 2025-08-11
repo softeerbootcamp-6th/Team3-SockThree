c1f5574e8c76b2d77259a42605be1d7692ba88c8
hkjbrian <hkjbrian@naver.com>
2025-08-11 17:46:05 +0900

    feat: WidgetSpec record 수정 - height, width 삭제 - WidgetSize enum 추가

--- server/src/main/java/com/seniclass/server/domain/lecture/dto/WidgetSpec.java
+++ server/src/main/java/com/seniclass/server/domain/lecture/dto/WidgetSpec.java
@@ -1,11 +1,11 @@
 package com.seniclass.server.domain.lecture.dto;
 
+import com.seniclass.server.domain.lecture.enums.WidgetSize;
 import com.seniclass.server.domain.lecture.enums.WidgetType;
 
 public record WidgetSpec(
         WidgetType type,
         Integer row,
         Integer col,
-        Integer height,
-        Integer width,
+        WidgetSize widgetSize,
         Boolean visible) {}
