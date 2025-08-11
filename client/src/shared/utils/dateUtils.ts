8808a4d0fc7e44cf36b46111f696dae46ecd4ed8
yyeonzu <yyeonzuu@gmail.com>
2025-08-11 15:49:03 +0900

    feat: formatDate 유틸 함수 구현 - 파라미터로 하이픈과 온점을 받아서, YY-MM-DD 혹은 YY.MM.DD 형식으로 리턴할 수 있습니다.

---
+++ client/src/shared/utils/dateUtils.ts
@@ -0,0 +1,17 @@
+// Date 객체 format과 관련한 함수
+export function formatDate(
+  dateInput: Date | string | number,
+  delimiter: "-" | "."
+): string {
+  const date = dateInput instanceof Date ? dateInput : new Date(dateInput);
+
+  if (isNaN(date.getTime())) {
+    throw new Error("Invalid date");
+  }
+
+  const yy = String(date.getFullYear()).slice(-2);
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const dd = String(date.getDate()).padStart(2, "0");
+
+  return `${yy}${delimiter}${mm}${delimiter}${dd}`;
+}
