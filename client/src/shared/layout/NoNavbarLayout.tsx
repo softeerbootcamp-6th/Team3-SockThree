e057572d3f3729561abbfa9bbc2e2285aaf98223
yyeonzu <yyeonzuu@gmail.com>
2025-08-11 16:34:11 +0900

    refactor: 상단 로고 영역을 sticky로 변경

--- client/src/shared/layout/NoNavbarLayout.tsx
+++ client/src/shared/layout/NoNavbarLayout.tsx
@@ -4,7 +4,9 @@ import Logo from "@/assets/logo/logo.svg?react";
 const NoNavbarLayout = () => {
   return (
     <div className="relative flex h-screen w-full flex-col">
-      <Logo className="absolute top-[4.5625rem] left-[2.625rem] h-[3rem] w-[6.8125rem]" />
+      <header className="sticky top-0 left-[2.625rem] z-50 bg-bg py-[1rem] pl-[1.1rem]">
+        <Logo className="h-[3rem] w-[6.8125rem]" />
+      </header>
       <Outlet />
     </div>
   );
