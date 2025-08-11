a1fe7d498ee454f655378c2288481fd7d5e85578
hkjbrian <hkjbrian@naver.com>
2025-08-11 20:24:20 +0900

    docs: swagger 문서 추가

--- server/src/main/java/com/seniclass/server/domain/lecture/dto/WidgetSpec.java
+++ server/src/main/java/com/seniclass/server/domain/lecture/dto/WidgetSpec.java
@@ -2,10 +2,24 @@ package com.seniclass.server.domain.lecture.dto;
 
 import com.seniclass.server.domain.lecture.enums.WidgetSize;
 import com.seniclass.server.domain.lecture.enums.WidgetType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+import static com.seniclass.server.domain.lecture.enums.WidgetType.*;
+
+@Schema(description = "위젯 스펙 DTO")
 public record WidgetSpec(
-        WidgetType type,
-        Integer row,
-        Integer col,
-        WidgetSize widgetSize,
-        Boolean visible) {}
+        @Schema(description = "위젯 타입", example = "ATTENDANCE" , allowableValues = {
+                "QNA",
+                "SUBMISSION",
+                "TEACHER_INFO",
+                "REVIEW",
+                "NEXT_LECTURE",
+                "STUDENTS_STATUS"
+        }) WidgetType type,
+        @Schema(description = "행 위치", example = "1") Integer row,
+        @Schema(description = "열 위치", example = "1") Integer col,
+        @Schema(description = "위젯 크기", example = "SMALL", allowableValues = {
+                "SMALL",
+                "LARGE"
+        }) WidgetSize widgetSize,
+        @Schema(description = "표시 여부", example = "true") Boolean visible) {}
--- server/src/main/java/com/seniclass/server/domain/lecture/dto/request/WidgetSettingUpdateRequest.java
+++ server/src/main/java/com/seniclass/server/domain/lecture/dto/request/WidgetSettingUpdateRequest.java
@@ -1,9 +1,12 @@
 package com.seniclass.server.domain.lecture.dto.request;
 
 import com.seniclass.server.domain.lecture.dto.WidgetSpec;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.LinkedHashMap;
 
+@Schema(description = "위젯 설정 수정 요청 DTO")
 public record WidgetSettingUpdateRequest(
+        @Schema(description = "위젯 설정 맵 (Key: WidgetSettingId, Value: 위젯 스펙)")
         LinkedHashMap<Long, WidgetSpec> widgetSettings
         ) {}
