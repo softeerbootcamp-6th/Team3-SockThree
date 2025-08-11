e9d18d4ac73aaa25515fe6451bbd351900dda17d
choi-jiae <chlwldo2002@gmail.com>
2025-08-12 00:27:22 +0900

    style: 기본 모달 위치, rounded 수정

--- client/src/shared/components/Modal.tsx
+++ client/src/shared/components/Modal.tsx
@@ -6,7 +6,10 @@ interface ModalProps {
 
 const Modal = forwardRef<HTMLDialogElement, ModalProps>(({ children }, ref) => {
   return (
-    <dialog ref={ref} className="backdrop:bg-black/60">
+    <dialog
+      ref={ref}
+      className="fix top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-[1.25rem] bg-white backdrop:bg-black/60"
+    >
       {children}
     </dialog>
   );
