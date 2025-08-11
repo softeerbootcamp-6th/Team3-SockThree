bad3d364db484b47f887744435f02bdbefb44e34
coffeesigma <ljb22060@gmail.com>
2025-08-11 13:49:42 +0900

    feat: 파일 관련 유틸 추가

--- server/src/main/java/com/seniclass/server/global/exception/errorcode/GlobalErrorCode.java
+++ server/src/main/java/com/seniclass/server/global/exception/errorcode/GlobalErrorCode.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum GlobalErrorCode implements BaseErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류입니다. 관리자에게 문의해주세요."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다. 요청 형식을 확인해주세요."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
