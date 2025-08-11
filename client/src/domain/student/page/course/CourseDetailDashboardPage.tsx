680832530e8339383bdeeba92dc6ecbd1f149666
yyeonzu <yyeonzuu@gmail.com>
2025-08-11 21:31:07 +0900

    chore: Dashboard 영역에 목 컴포넌트 렌더링

--- client/src/domain/student/page/course/CourseDetailDashboardPage.tsx
+++ client/src/domain/student/page/course/CourseDetailDashboardPage.tsx
@@ -1,17 +1,40 @@
-// src/pages/CourseDetailPage.tsx
-
-// import CourseHero from "@/domain/course/components/CourseHero";
-// import CourseTabs from "@/domain/course/components/CourseTabs";
-// import InstructorCard from "@/domain/course/components/InstructorCard";
-// import CourseStatList from "@/domain/course/components/CourseStatList";
-// import RichText from "@/shared/components/RichText";
-// import ApplySidebar from "@/domain/course/components/ApplySidebar";
-
-// import { CourseInfoCardDefault } from "@/domain/student/component/course/CourseInfoCardDefault";
-// import CourseInfoCard from "@/shared/components/course/CourseInfoCard";
+import StudentStatsWidget from "@/domain/student/component/widgets/StudentStatsWidget";
+import InstructorInfoWidget from "@/domain/student/component/widgets/InstructorInfoWidget";
 
 const CourseDetailDashboardPage = () => {
-  return <></>;
+  return (
+    <div className="flex w-full flex-col gap-[1rem]">
+      <div className="flex flex-row gap-[1rem]">
+        <StudentStatsWidget
+          size="large"
+          ageData={[
+            { subject: "50세", male: 10, female: 15, fullMark: 40 },
+            { subject: "55세", male: 18, female: 20, fullMark: 40 },
+            { subject: "60세", male: 20, female: 8, fullMark: 40 },
+            { subject: "65세", male: 15, female: 10, fullMark: 40 },
+            { subject: "70세", male: 12, female: 2, fullMark: 40 },
+            { subject: "75세", male: 5, female: 1, fullMark: 40 },
+          ]}
+          homeworkData={[
+            { name: "2회", value: 100 },
+            { name: "3회", value: 50 },
+            { name: "4회", value: 60 },
+            { name: "5회", value: 60 },
+            { name: "6회", value: 20 },
+          ]}
+        />
+        <InstructorInfoWidget size="small" />
+      </div>
+      <div className="flex flex-row gap-[1rem]">
+        <InstructorInfoWidget size="large" />
+        <StudentStatsWidget size="small" />
+      </div>
+      <div className="flex flex-row gap-[1rem]">
+        <StudentStatsWidget size="small" />
+        <InstructorInfoWidget size="large" />
+      </div>
+    </div>
+  );
 };
 
 export default CourseDetailDashboardPage;
