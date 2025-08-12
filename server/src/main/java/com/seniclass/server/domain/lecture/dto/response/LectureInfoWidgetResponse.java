9183f62df7f66028d35e3a5a760e3766de4663e0
hkjbrian <hkjbrian@naver.com>
2025-08-12 17:58:55 +0900

    feat: 조회 응답 dto 생성 - 강좌 정보 위젯에 해당하는 정보를 반환하는 LectureInfoWidgetResponse 생성 - 강좌의 업로드 시간에 해당하는 정보를 반환하는 UploadTimeResponse 생성

---
+++ server/src/main/java/com/seniclass/server/domain/lecture/dto/response/LectureInfoWidgetResponse.java
@@ -0,0 +1,30 @@
+package com.seniclass.server.domain.lecture.dto.response;
+
+import com.seniclass.server.domain.lecture.domain.Lecture;
+import com.seniclass.server.domain.lecture.enums.Level;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record LectureInfoWidgetResponse(
+        Long lectureId,
+        Integer cohort,
+        List<UploadTimeResponse> uploadTimesList,
+        LocalDate start,
+        LocalDate endDate,
+        Level level,
+        Integer fee
+) {
+    public static LectureInfoWidgetResponse from(
+            Lecture lecture,
+            List<UploadTimeResponse> uploadTimesList) {
+        return new LectureInfoWidgetResponse(
+                lecture.getId(),
+                lecture.getCohort(),
+                uploadTimesList,
+                lecture.getStartDate(),
+                lecture.getEndDate(),
+                lecture.getLevel(),
+                lecture.getFee());
+    }
+}
