739f0cb54acda6da08fd864bac5661d06bd24f6f
hkjbrian <hkjbrian@naver.com>
2025-08-12 10:43:30 +0900

    docs: WidgetSettingUpdateRequest dto 제한 조건 추가

--- server/src/main/java/com/seniclass/server/domain/lecture/dto/request/WidgetSettingUpdateRequest.java
+++ server/src/main/java/com/seniclass/server/domain/lecture/dto/request/WidgetSettingUpdateRequest.java
@@ -2,9 +2,12 @@ package com.seniclass.server.domain.lecture.dto.request;
 
 import com.seniclass.server.domain.lecture.dto.WidgetSpec;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.LinkedHashMap;
 
 @Schema(description = "위젯 설정 수정 요청 DTO")
 public record WidgetSettingUpdateRequest(
         @Schema(description = "위젯 설정 맵 (Key: WidgetSettingId, Value: 위젯 스펙)")
-                LinkedHashMap<Long, WidgetSpec> widgetSettings) {}
+                @NotNull(message = "위젯 세팅 값은 필수입니다.") @NotEmpty(message = "위젯 세팅 값은 비어있을 수 없습니다.") LinkedHashMap<Long, WidgetSpec> widgetSettings) {}
