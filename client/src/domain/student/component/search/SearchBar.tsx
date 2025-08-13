0b393933d29fb71a39b0507dc788cb960191a410
yyeonzu <yyeonzuu@gmail.com>
2025-08-13 22:47:11 +0900

    feat: value 값 디바운싱하는 훅 추가

--- client/src/domain/student/component/search/SearchBar.tsx
+++ client/src/domain/student/component/search/SearchBar.tsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+
 interface SearchBarProps {
   value: string;
   onChange: (q: string) => void;
 }
+
+export const useDebouncedValue = (value: string, delayMs: number) => {
+  const [debounced, setDebounced] = useState(value);
+  useEffect(() => {
+    const id = setTimeout(() => setDebounced(value), delayMs);
+    return () => clearTimeout(id);
+  }, [value, delayMs]);
+  return debounced;
+};
 
 const SearchBar = ({ value, onChange }: SearchBarProps) => {
   return (
