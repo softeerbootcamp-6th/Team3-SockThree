11553aca7fe6e61b7da37d07b0354e70c1e7b28c
yyeonzu <yyeonzuu@gmail.com>
2025-08-14 02:27:44 +0900

    feat: url에 쿼리스트링 추가

--- client/src/domain/student/page/course/SearchResultPage.tsx
+++ client/src/domain/student/page/course/SearchResultPage.tsx
@@ -1,27 +1,68 @@
 import SearchBar from "@/domain/student/component/search/SearchBar";
 import FilterBar from "@/domain/student/component/search/FilterBar";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
 
 const SearchResultPage = () => {
-  const [searchQuery, setSearchQuery] = useState<string>();
-  const [selectedCategory, setSelectedCategory] = useState<string>();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [selectedCategory, setSelectedCategory] = useState<
+    string | undefined
+  >();
   const [selectedSubcategories, setSelectedSubcategories] = useState<string[]>(
     []
   );
 
+  useEffect(() => {
+    const query = searchParams.get("q") || "";
+    const category = searchParams.get("category") || undefined;
+    const subcategories =
+      searchParams.get("subcategories")?.split(",").filter(Boolean) || [];
+
+    setSearchQuery(query);
+    setSelectedCategory(category);
+    setSelectedSubcategories(subcategories);
+  }, []);
+
+  const updateSearchParams = useCallback(
+    (query: string, category?: string, subcategories: string[] = []) => {
+      const params = new URLSearchParams(searchParams);
+      if (params.toString() !== searchParams.toString()) {
+        setSearchParams(params);
+      }
+
+      if (query.trim()) {
+        params.set("q", query);
+      }
+
+      if (category) {
+        params.set("category", category);
+      }
+
+      if (subcategories.length > 0) {
+        params.set("subcategories", subcategories.join(","));
+      }
+
+      setSearchParams(params);
+    },
+    [searchParams, setSearchParams]
+  );
+
   const handleSearchInputChange = (q: string) => {
     setSearchQuery(q);
+    updateSearchParams(q, selectedCategory, selectedSubcategories);
   };
 
-  const handleCategoryChange = (category: string) => {
+  const handleCategoryChange = (category: string | undefined) => {
     setSelectedCategory(category);
+    updateSearchParams(searchQuery ?? "", category, selectedSubcategories);
   };
 
   const handleSubcategoryChange = (subcategories: string[]) => {
     setSelectedSubcategories(subcategories);
+    updateSearchParams(searchQuery ?? "", selectedCategory, subcategories);
   };
-
-  console.log(searchQuery, selectedCategory, selectedSubcategories);
 
   return (
     <div className="mt-[10rem] w-[100rem]">
