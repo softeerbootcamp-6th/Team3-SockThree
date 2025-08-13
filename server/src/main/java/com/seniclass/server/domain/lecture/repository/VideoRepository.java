b3890f445500715f9e30ee82050a9f1821bf756e
hkjbrian <hkjbrian@naver.com>
2025-08-13 15:53:27 +0900

    docs: spotlessApply

--- server/src/main/java/com/seniclass/server/domain/lecture/repository/VideoRepository.java
+++ server/src/main/java/com/seniclass/server/domain/lecture/repository/VideoRepository.java
@@ -1,7 +1,6 @@
 package com.seniclass.server.domain.lecture.repository;
 
 import com.seniclass.server.domain.lecture.domain.Video;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
