d89b6fe21e8e93149d0d4fb2985a37eac2f51e7a
hkjbrian <hkjbrian@naver.com>
2025-08-13 15:53:08 +0900

    feat: dto 생성자 추가 및 swagger 문서 설정

--- server/src/main/java/com/seniclass/server/domain/lecture/dto/response/MyLectureStatusWidgetResponse.java
+++ server/src/main/java/com/seniclass/server/domain/lecture/dto/response/MyLectureStatusWidgetResponse.java
@@ -1,38 +1,41 @@
 package com.seniclass.server.domain.lecture.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 
+@Schema(description = "내 강의 현황 위젯 응답")
 public record MyLectureStatusWidgetResponse(
-        Integer LectureProgressRate,
-        Integer TotalLectureLengthHour,
-        Integer TotalVideoCount,
-        LocalDateTime recentAssignmentDueDateTime,
-        String AssignmentName,
-        Boolean isAssignmentSubmitted) {
-    public static MyLectureStatusWidgetResponse of(
-            Integer lectureProgressRate, Long totalLectureDuration, Integer totalVideoCount) {
-        return new MyLectureStatusWidgetResponse(
-                lectureProgressRate,
-                (int) (totalLectureDuration / 3600), // Convert seconds to hours
-                totalVideoCount,
-                null,
-                null,
-                null);
-    }
-
+        @Schema(description = "강의 진행률", example = "50") Integer lectureProgressRate,
+        @Schema(description = "강좌의 총 동영상 재생시간", example = "121") Integer totalVideoDurationHour,
+        @Schema(description = "총 동영상 개수", example = "10") Integer lectureVideoCount,
+        @Schema(description = "가장 가까운 과제 마감일", example = "2025-08-20T23:59:59")
+                LocalDateTime assignmentDueDateTime,
+        @Schema(description = "과제 이름", example = "1주차 과제") String assignmentName,
+        @Schema(description = "과제 제출 여부", example = "true") Boolean isAssignmentSubmitted) {
     public static MyLectureStatusWidgetResponse of(
             Integer lectureProgressRate,
-            Long totalLectureDuration,
-            Integer totalVideoCount,
-            LocalDateTime recentAssignmentDueDateTime,
+            Long totalVideoDurationHour,
+            Integer lectureVideoCount,
+            LocalDateTime assignmentDueDateTime,
             String assignmentName,
             Boolean isAssignmentSubmitted) {
         return new MyLectureStatusWidgetResponse(
                 lectureProgressRate,
-                (int) (totalLectureDuration / 3600), // Convert seconds to hours
-                totalVideoCount,
-                recentAssignmentDueDateTime,
+                (int) (totalVideoDurationHour / 3600),
+                lectureVideoCount,
+                assignmentDueDateTime,
                 assignmentName,
                 isAssignmentSubmitted);
+    }
+
+    public static MyLectureStatusWidgetResponse of(
+            Integer lectureProgressRate, Long totalVideoDurationHour, Integer lectureVideoCount) {
+        return new MyLectureStatusWidgetResponse(
+                lectureProgressRate,
+                (int) (totalVideoDurationHour / 3600),
+                lectureVideoCount,
+                null,
+                null,
+                null);
     }
 }
