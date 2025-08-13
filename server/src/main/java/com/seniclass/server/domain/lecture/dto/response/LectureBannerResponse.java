588c3ae96fe097ce2a2d1d70e893b62af4a522e2
hkjbrian <hkjbrian@naver.com>
2025-08-13 18:54:24 +0900

    docs: spotless Apply

--- server/src/main/java/com/seniclass/server/domain/lecture/dto/response/LectureBannerResponse.java
+++ server/src/main/java/com/seniclass/server/domain/lecture/dto/response/LectureBannerResponse.java
@@ -5,8 +5,9 @@ import com.seniclass.server.domain.category.domain.SubCategory;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record LectureBannerResponse(
-        @Schema(description = "강좌 id" , example = "1") Long lectureId,
-        @Schema(description = "강좌 이미지 URL", example = "www.aws.com/s3/image/1") String presignedImageURL,
+        @Schema(description = "강좌 id", example = "1") Long lectureId,
+        @Schema(description = "강좌 이미지 URL", example = "www.aws.com/s3/image/1")
+                String presignedImageURL,
         @Schema(description = "강좌명", example = "골프 왕초보 강좌") String name,
         @Schema(description = "기수", example = "1") Integer cohort,
         @Schema(description = "찜 개수", example = "53") Integer bookMarkCount,
@@ -14,7 +15,7 @@ public record LectureBannerResponse(
         @Schema(description = "현재 수강 인원", example = "17") Integer enrolledStudentCount,
         @Schema(description = "메인 카테고리", example = "운동") String mainCategoryName,
         @Schema(description = "보조 카테고리", example = "골프") String subCategoryName,
-        @Schema(description = "강좌 수강 기간 (개월)", example = "2")Integer lectureDuration) {
+        @Schema(description = "강좌 수강 기간 (개월)", example = "2") Integer lectureDuration) {
     public static LectureBannerResponse of(
             Long lectureId,
             String presignedImageURL,
