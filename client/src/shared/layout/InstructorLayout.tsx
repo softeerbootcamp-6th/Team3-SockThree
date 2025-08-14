40076664621575457791a7e6fc4803a43520a554
choi-jiae <chlwldo2002@gmail.com>
2025-08-14 11:28:04 +0900

    fix: instructor layout gap으로 수정

--- client/src/shared/layout/InstructorLayout.tsx
+++ client/src/shared/layout/InstructorLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router";
 
 const InstructorLayout = () => {
   return (
-    <div className="flex h-screen flex-row justify-between">
+    <div className="flex h-screen flex-row gap-3">
       <InstructorNavbar />
       <Outlet />
     </div>
