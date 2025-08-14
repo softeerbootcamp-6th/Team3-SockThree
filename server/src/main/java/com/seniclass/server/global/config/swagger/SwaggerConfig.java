24c596ba223dc72c402343c4d624aec193a618a1
coffeesigma <ljb22060@gmail.com>
2025-08-15 02:34:38 +0900

    fix: add Authorization field

--- server/src/main/java/com/seniclass/server/global/config/swagger/SwaggerConfig.java
+++ server/src/main/java/com/seniclass/server/global/config/swagger/SwaggerConfig.java
@@ -1,7 +1,12 @@
 package com.seniclass.server.global.config.swagger;
 
+import static io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP;
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +19,15 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .servers(List.of(new Server().url("/")))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        "bearerAuth",
+                                        new SecurityScheme()
+                                                .type(HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .info(
                         new Info()
                                 .title("Senicle Service API") // 서비스 이름
