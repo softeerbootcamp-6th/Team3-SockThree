18e8f324cb6f3e3df18eb809f5484b895e530911
hkjbrian <hkjbrian@naver.com>
2025-08-14 14:38:09 +0900

    feat: 리뷰 관련 쿼리 생성 - 특정 강의의 평균 리뷰 점수 조회 - 특정 강사의 총 리뷰 개수 조회

--- server/src/main/java/com/seniclass/server/domain/lecture/repository/ReviewRepository.java
+++ server/src/main/java/com/seniclass/server/domain/lecture/repository/ReviewRepository.java
@@ -2,7 +2,17 @@ package com.seniclass.server.domain.lecture.repository;
 
 import com.seniclass.server.domain.lecture.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, Long> {}
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    // * 강의 ID로 평균 평점 조회 */
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.lecture.id = :lectureId")
+    Double findAverageRatingByLectureId(Long lectureId);
+
+    // * 강사 ID로 리뷰 개수 조회 */
+    @Query("SELECT COUNT(r) FROM Review r WHERE r.lecture.teacher.id = :teacherId")
+    Integer countByTeacherId(Long teacherId);
+}
