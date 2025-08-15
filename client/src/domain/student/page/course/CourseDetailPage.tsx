0d5cd23c858e24536d6a90b34602075a81072432
yyeonzu <yyeonzuu@gmail.com>
2025-08-15 14:23:43 +0900

    fix: 탭 내에 대시보드, 커리큘럼 페이지 추가

--- client/src/domain/student/page/course/CourseDetailPage.tsx
+++ client/src/domain/student/page/course/CourseDetailPage.tsx
@@ -3,6 +3,10 @@ import CourseInfoCardAttend from "@/domain/student/component/course/CourseInfoCa
 import CourseDetailWrapper from "@/shared/components/course/CourseDetailWrapper";
 import CourseInfoCardDefault from "@/domain/student/component/course/CourseInfoCardDefault";
 
+//"/course/:courseId/*" 이내의 페이지
+import CourseDetailDashboardPage from "@/domain/student/page/course/CourseDetailDashboardPage";
+import CourseDetailVideoListPage from "@/domain/student/page/course/CourseDetailVideoListPage";
+
 const CourseDetailPage = () => {
   const location = useLocation();
   const isDashboard = location.pathname.includes("/dashboard");
@@ -21,9 +25,9 @@ const CourseDetailPage = () => {
       {/*  탭 내부 라우팅 */}
       <Routes>
         <Route index element={<div></div>} />
-        <Route path="curriculum" element={<div></div>} />
+        <Route path="curriculum" element={<CourseDetailVideoListPage />} />
         <Route path="reviews" element={<div></div>} />
-        <Route path="dashboard" element={<div></div>} />
+        <Route path="dashboard" element={<CourseDetailDashboardPage />} />
         <Route path="assignments" element={<div></div>} />
         <Route path="questions" element={<div></div>} />
       </Routes>
