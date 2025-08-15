074b61f55ce5a999be6b7ecd85706835e125c1d6
yyeonzu <yyeonzuu@gmail.com>
2025-08-15 15:24:43 +0900

    chore: prettier 적용

--- client/src/shared/layout/InstructorLayout.tsx
+++ client/src/shared/layout/InstructorLayout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router";
 
 const InstructorLayout = () => {
-    return (
-        <div>
-            {/*  Layout 추가 예정 */}
-            <Outlet />
-        </div>
-    );
+  return (
+    <div>
+      {/*  Layout 추가 예정 */}
+      <Outlet />
+    </div>
+  );
 };
 
 export default InstructorLayout;
