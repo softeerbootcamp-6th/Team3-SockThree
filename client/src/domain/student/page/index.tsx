b8aead869ef30dfe732d1ec50a215b6dcd0a6cb7
choi-jiae <chlwldo2002@gmail.com>
2025-08-15 14:29:39 +0900

    feat: curriculum 페이지 추가

--- client/src/domain/student/page/index.tsx
+++ client/src/domain/student/page/index.tsx
@@ -7,3 +7,6 @@ export { default as HeartPage } from "@/domain/student/page/mypage/HeartPage";
 export { default as CurrentCoursePage } from "@/domain/student/page/mypage/CurrentCoursePage";
 export { default as EndCoursePage } from "@/domain/student/page/mypage/EndCoursePage";
 export { default as CourseDetailDashboardPage } from "@/domain/student/page/course/CourseDetailDashboardPage";
+
+export { default as CourseCurriculumPage } from "@/domain/student/page/course/CourseCurriculumPage";
+export { default as VideoPlayPage } from "@/domain/student/page/course/video/VideoPlayPage";
