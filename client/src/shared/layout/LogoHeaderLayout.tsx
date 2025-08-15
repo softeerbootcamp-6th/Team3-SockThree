0dd65fda0b5b8580daf7d14015898149a4df9ca6
yyeonzu <yyeonzuu@gmail.com>
2025-08-15 14:23:25 +0900

    fix: 레이아웃 width를 max에서 고정으로 수정

--- client/src/shared/layout/LogoHeaderLayout.tsx
+++ client/src/shared/layout/LogoHeaderLayout.tsx
@@ -7,7 +7,7 @@ const LogoHeaderLayout = () => {
       <header className="sticky top-0 left-[2.625rem] z-50 flex h-fit bg-bg py-[1rem] pl-[1rem]">
         <Logo className="h-[3rem] w-[6.8125rem]" />
       </header>
-      <main className="mx-auto flex max-w-[1720px] min-w-[1280px] flex-col items-center justify-center">
+      <main className="mx-auto flex w-[1280px] flex-col items-center justify-center">
         <Outlet />
       </main>
     </div>
