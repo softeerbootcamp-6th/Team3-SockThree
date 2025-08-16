8c67f0b2ba436e0899dc96e6f1850d8075600a45
yyeonzu <yyeonzuu@gmail.com>
2025-08-16 16:38:18 +0900

    feat: token이 없는 경우 login으로 리다이렉트

--- client/src/shared/auth/checkAuth.ts
+++ client/src/shared/auth/checkAuth.ts
@@ -1,11 +1,28 @@
 import { redirect } from "react-router";
 import type { UserType } from "@/shared/types/auth";
+import { token } from "@/shared/api/token";
+import { validate } from "@/shared/api/auth";
 
 export const checkAuth = async () => {
-  const isAuthenticated = true; // @TODO: 인증 로직
-  if (!isAuthenticated) throw redirect("/login");
+  const isToken = token.has();
+  if (!isToken) {
+    throw redirect("/login");
+  }
+  // @TODO validate에 따른 인증 로직 추가
 
-  const userType: UserType = "student"; // @TODO: 권한 체크 로직
-
+  // @TODO validate응답에 인가 포함 로직 추가
+  const userType: UserType = "student";
   return { userType };
+
+  try {
+    console.log("validate API 호출 시작");
+    const res = await validate();
+    console.log(res);
+    const userType: UserType = "student";
+    return { userType };
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_err) {
+    token.clear();
+    throw redirect("/login");
+  }
 };
